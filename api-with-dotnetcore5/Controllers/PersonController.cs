d0d008f945114c1af367f537c15c335787bacb80
DIEGO HARRISON DE SOUSA MESQUITA <diego.mesquita.e@midway.com.br>
2022-01-17 22:55:25 -0300

    structure modify

---
+++ api-with-dotnetcore5/Controllers/PersonController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace RestWithASPNETUdemy.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PersonController : ControllerBase
+    {
+
+        private readonly ILogger<CalculatorController> _logger;
+        public PersonController(ILogger<CalculatorController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("sum/{firstNumber}/{secondNumber}")]
+        public IActionResult Sum(string firstNumber, string secondNumber)
+        {            
+            return BadRequest("Invalid Input");
+        }        
+    }
+}
